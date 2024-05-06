73f470e20d533739bfe9c3776d01210d53e8ea27
Aman Tripathi <amanttripathi02@gmail.com>
2024-05-06 12:52:37 +0530

    "building a tree from level order traversal"

--- cpp files/cpp4.cpp
+++ cpp files/cpp4.cpp
@@ -59,12 +59,94 @@ void LevelOrderTraversal(node *root)
         }
     }
 }
+void Inorder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    Inorder(root->left);
+    cout << root->data << " ";
+    Inorder(root->right);
+}
+
+void preOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void postOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+void buildTreefromLevelOrder(node *&root)
+{
+    queue<node *> q;
+    int data;
+    cin >> data;
+
+    root = new node(data);
+    q.push(root);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        cout << "enter the left node for : " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if (leftData != -1)
+        {
+            temp->left = new node(leftData);
+            q.push(temp->left);
+        }
+        cout << "enter the right node for : " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if (rightData != -1)
+        {
+            temp->right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
 
 int main()
 {
     node *root = NULL;
     // creating the tree
     root = buildTree(root);
+
+    cout << "Levelorder Traversal of tree is:" << endl;
+    LevelOrderTraversal(root);
+
+    cout << endl;
+    cout << "Inorder Traversal of tree is:" << endl;
+    Inorder(root);
+    cout << endl;
+
+    cout << "PreOrder Traversal of tree is:" << endl;
+    preOrder(root);
+    cout << endl;
+
+    cout << "Postorder Traversal of tree is:" << endl;
+    postOrder(root);
+    cout << endl;
 
     return 0;
 }
