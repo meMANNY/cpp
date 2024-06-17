816c3a09e40cf7de1511a778ecf7686b790d5f43
Aman Tripathi <amanttripathi02@gmail.com>
2024-06-17 09:59:08 +0530

    Created a template for segment tree in cpp

--- cpp files/segment_tree.cpp
+++ cpp files/segment_tree.cpp
@@ -2,68 +2,120 @@
 
 using namespace std;
 
-void build(int ind, int low, int high, int arr[], int seg[])
+class SegmentTree
 {
-    if (low == high)
+    vector<int> seg;
+
+public:
+    SegmentTree(int n)
     {
-        seg[ind] = arr[low];
-        return;
+        seg.resize(4 * n + 1);
+    }
+    void build(int ind, int low, int high, int arr[])
+    {
+        if (low == high)
+        {
+            seg[ind] = arr[low];
+            return;
+        }
+
+        int mid = (low + high) / 2;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
+
+        seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
+    }
+    int query(int ind, int low, int high, int l, int r)
+    {
+
+        if (r < low || l > high)
+        {
+            return INT_MAX;
+        }
+        if (l <= low && r >= high)
+        {
+            return seg[ind];
+        }
+
+        int mid = (low + high) / 2;
+        int left = query(2 * ind + 1, low, high, l, r);
+        int right = query(2 * ind + 2, low, high, l, r);
+
+        return min(left, right);
     }
 
-    int mid = (low + high) / 2;
-    build(2 * ind + 1, low, mid, arr, seg);
-    build(2 * ind + 2, mid + 1, high, arr, seg);
+    void update(int ind, int low, int high, int i, int val)
+    {
+        if (i < low || i > high)
+        {
+            return;
+        }
+        if (low == high)
+        {
+            seg[ind] = val;
+            return;
+        }
 
-    seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
-}
-int query(int ind, int low, int high, int l, int r, int seg[])
+        int mid = (low + high) / 2;
+        update(2 * ind + 1, low, mid, i, val);
+        update(2 * ind + 2, mid + 1, high, i, val);
+
+        seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
+    }
+};
+
+// void solve()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++)
+//         cin >> arr[i];
+//     int seg[4 * n];
+//     build(0, 0, n - 1, arr,seg);
+
+//     int q;
+//     cin >> q;
+//     while (q--)
+//     {
+//         int type;
+//         cin >> type;
+//         if (type == 1)
+//         {
+//             int l, r;
+//             cin >> l >> r;
+//             cout << query(0, 0, n - 1, l, r, seg) << endl;
+//         }
+//         else
+//         {
+//             int i, val;
+//             cin >> i >> val;
+//             update(0, 0, n - 1, i, val, seg);
+//             arr[i] = val;
+//         }
+//     }
+// }
+void solve2()
 {
-
-    if (r < low || l > high)
+    int n1;
+    cin >> n1;
+    int arr[n1];
+    for (int i = 0; i < n1; i++)
     {
-        return INT_MAX;
-    }
-    if (l <= low && r >= high)
-    {
-        return seg[ind];
-    }
-
-    int mid = (low + high) / 2;
-    int left = query(2 * ind + 1, low, high, l, r, seg);
-    int right = query(2 * ind + 2, low, high, l, r, seg);
-
-    return min(left, right);
-}
-
-void update(int ind, int low, int high, int i, int val, int seg[])
-{
-    if (i < low || i > high)
-    {
-        return;
-    }
-    if (low == high)
-    {
-        seg[ind] = val;
-        return;
-    }
-
-    int mid = (low + high) / 2;
-    update(2 * ind + 1, low, mid, i, val, seg);
-    update(2 * ind + 2, mid + 1, high, i, val, seg);
-
-    seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
-}
-
-void solve()
-{
-    int n;
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
         cin >> arr[i];
-    int seg[4 * n];
-    build(0, 0, n - 1, arr, seg);
+    }
+    SegmentTree st1(n1);
+    st1.build(0, 0, n1 - 1, arr);
+    int n2;
+    cin >> n2;
+    int arr2[n2];
+    for (int i = 0; i < n2; i++)
+    {
+        cin >> arr2[i];
+    }
+    SegmentTree st2(n2);
+    st2.build(0, 0, n2 - 1, arr2);
 
     int q;
     cin >> q;
@@ -73,25 +125,39 @@ void solve()
         cin >> type;
         if (type == 1)
         {
-            int l, r;
-            cin >> l >> r;
-            cout << query(0, 0, n - 1, l, r, seg) << endl;
+            int l1, r1, l2, r2;
+
+            cin >> l1 >> r1 >> l2 >> r2;
+            int mini1 = st1.query(0, 0, n1 - 1, l1, r1);
+            int mini2 = st2.query(0, 0, n2 - 1, l2, r2);
+
+            cout << min(mini1, mini2) << endl;
         }
         else
         {
-            int i, val;
-            cin >> i >> val;
-            update(0, 0, n - 1, i, val, seg);
-            arr[i] = val;
+            int arrNo, ind, val;
+            cin >> arrNo >> ind >> val;
+
+            if (arrNo == 1)
+            {
+                st1.update(0, 0, n1 - 1, ind, val);
+                arr[ind] = val;
+            }
+            else
+            {
+                st2.update(0, 0, n2 - 1, ind, val);
+                arr2[ind] = val;
+            }
         }
     }
 }
+
 signed main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    solve();
+    solve2();
     return 0;
 }
